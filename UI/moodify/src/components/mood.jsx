bd3b5f9f5f23fc756feb95d684303534324b1bb9
LifeIsFiction5 <jacob_barrett@icloud.com>
2024-10-16 22:54:44 -0500

    Add server

---
+++ UI/moodify/src/components/mood.jsx
@@ -0,0 +1,7 @@
+export default function Mood(selection) {
+    return(
+        <div>
+            <h2>How are you feeling?</h2>
+        </div>
+    );
+}
---
+++ UI/moodify/src/components/userLogIn.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (username === 'user' && password === 'password') {
+            console.log('Login successful')
+        }
+    }
+}
